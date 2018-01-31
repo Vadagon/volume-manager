0e6a737492ef51ea3f0bc25336cdfd9655581ed4
Vadagon <l3adagon@gmail.com>
2018-01-31 11:01:42 +0200

    v3

--- background.js
+++ background.js
@@ -1,67 +1,17 @@
 var gainNode, audioCtx, streamer, 
-gainLevels = {
-	1: 0,
-	2: 0.2,
-	3: 0.4,
-	4: 0.6,
-	5: 0.8,
-	6: 1,
-	7: 8
-
-}, tabsLevels = {}, tabsGaines = {}, hotkeysType = [true, false];
-chrome.storage.sync.get(["gainLevels", "hotkeysType"], function(items) {
+tabsLevels = {}, tabsGaines = {}, hotkeysType = [true, false];
+chrome.storage.sync.get(["hotkeysType"], function(items) {
 	if (!chrome.runtime.error) {
-		if(items.hasOwnProperty("gainLevels") && items.hasOwnProperty("hotkeysType")){
-			gainLevels = items.gainLevels;
+		if(items.hasOwnProperty("hotkeysType")){
 			hotkeysType = items.hotkeysType;
 		}else{
-			chrome.storage.sync.set({ "gainLevels" : gainLevels, "hotkeysType" : hotkeysType });
+			chrome.storage.sync.set({ "hotkeysType" : hotkeysType });
 		}
 	}
 });
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-    chrome.tabs.get(activeInfo.tabId, function (tab) {
-        mySuperCallback(tab.url);
-    });
-});
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, updatedTab) {
-    chrome.tabs.query({'active': true}, function (activeTabs) {
-        var activeTab = activeTabs[0];
-
-        if (activeTab == updatedTab) {
-            mySuperCallback(activeTab.url);
-        }
-    });
-});
-
-// Код на всякий случай, если нужно будет изменять уровни звука относительно ОСи
-// chrome.runtime.getPlatformInfo(function(info) {
-//     // Display host OS in the console
-//     console.log(info.os);
-//     os = info.os;
-// });
-
 
 // function for icons when user switch tabs
-function mySuperCallback(newUrl) {
-	chrome.tabs.query({ currentWindow: true, active: true }, function (tabArray) {
-		// chrome.browserAction.setIcon({path: icon});
-		if(tabsLevels.hasOwnProperty(tabArray[0].id)){
-			if(0.7 < gainLevels[tabsLevels[tabArray[0].id]] && 1 >= gainLevels[tabsLevels[tabArray[0].id]]) {
-			    chrome.browserAction.setIcon({path: 'images/lvl3.png'});
-			}else if(0.2 < gainLevels[tabsLevels[tabArray[0].id]] && 0.7 >= gainLevels[tabsLevels[tabArray[0].id]]){
-				chrome.browserAction.setIcon({path: 'images/lvl2.png'});
-			}else if(0 <= gainLevels[tabsLevels[tabArray[0].id]] && 0.2 >= gainLevels[tabsLevels[tabArray[0].id]]){
-				chrome.browserAction.setIcon({path: 'images/lvl1.png'});
-			}else if(gainLevels[tabsLevels[tabArray[0].id]] > 1){
-				chrome.browserAction.setIcon({path: 'images/lvl4.png'});
-			}
-		}
 
-		if(!tabsLevels.hasOwnProperty(tabArray[0].id))
-		    chrome.browserAction.setIcon({path: 'images/off.png'});
-	});
-}
 function showVolumeInTabFunc(level){
 	var showVolumeInTab = "var x = document.querySelectorAll('#VadagonVolumeStatus .VadagonVolumeStatusElems > span'); for (var i = "+12+" - 1; i >= 0; i--) {x[i].style.backgroundColor = '#1c1c1c';}";
 	showVolumeInTab = showVolumeInTab + 
@@ -88,96 +38,91 @@ chrome.tabs.onRemoved.addListener((a) => {
 });
 function mainClicker(e){
 	chrome.tabs.query({ currentWindow: true, active: true }, function (tabArray) {
-		if(tabsLevels.hasOwnProperty(tabArray[0].id)) {
-			if (e==='up')
-				tabsLevels[tabArray[0].id] == Object.keys(gainLevels).length?tabsLevels[tabArray[0].id] = 1:tabsLevels[tabArray[0].id]++;
-
-			if (e==='down')
-				tabsLevels[tabArray[0].id] == 1?tabsLevels[tabArray[0].id] = Object.keys(gainLevels).length:tabsLevels[tabArray[0].id]--;
-
-			showVolumeInTabFunc(gainLevels[tabsLevels[tabArray[0].id]]);
-			
-			if(tabsGaines[tabArray[0].id] == 'normalMode'){
-				chrome.tabCapture.capture({
-				    audio: true,
-				    video: false
-				}, function(stream) {
-					tabsGaines[tabArray[0].id] = {};
-					tabsGaines[tabArray[0].id].audioCtx = new window.AudioContext;
-					tabsGaines[tabArray[0].id].streamer = stream;
-			        tabsGaines[tabArray[0].id].source = tabsGaines[tabArray[0].id].audioCtx.createMediaStreamSource(stream);
-			        tabsGaines[tabArray[0].id].nodeGain = tabsGaines[tabArray[0].id].audioCtx.createGain();
-			        tabsGaines[tabArray[0].id].source.connect(tabsGaines[tabArray[0].id].nodeGain);
-			        tabsGaines[tabArray[0].id].nodeGain.connect(tabsGaines[tabArray[0].id].audioCtx.destination);
-			        tabsGaines[tabArray[0].id].nodeGain.gain.value = parseFloat(gainLevels[tabsLevels[tabArray[0].id]]);
-				});
-			}else if (gainLevels[tabsLevels[tabArray[0].id]]==1) {
-				tabsGaines[tabArray[0].id].streamer.getAudioTracks().forEach(function(track) {
-		            track.stop();
-		        });
-				Object.prototype.hasOwnProperty.call(tabsGaines, tabArray[0].id) && tabsGaines[tabArray[0].id].audioCtx.close()
-		        .then(() => {
-					tabsGaines[tabArray[0].id] = 'normalMode';
-		        })
-		    }else{
-				tabsGaines[tabArray[0].id].nodeGain.gain.value = parseFloat(gainLevels[tabsLevels[tabArray[0].id]]);
-		    }
-
+		var id = tabArray[0].id;
+		if (a.getTab(id)){
+			var resFloat = parseInt(Math.floor(tabsLevels[id]*10)+e)/10;
+			if (tabsLevels[id] > 1 && e < 0)
+				resFloat = 1;
+			if (resFloat <= 0)
+				resFloat = 0;
+			if (resFloat > 1)
+				resFloat = 8;
+			console.log(resFloat);
+			tabsLevels[id] = resFloat;
+			showVolumeInTabFunc(tabsLevels[id]);
+			a.volume(id, tabsLevels[id]*100);
 		}else{
-			chrome.tabCapture.capture({
-			    audio: true,
-			    video: false
-			}, function(stream) {
-				showVolumeInTabFunc(gainLevels[1])
-				tabsLevels[tabArray[0].id] = 1;
-				tabsGaines[tabArray[0].id] = {};
-				tabsGaines[tabArray[0].id].audioCtx = new window.AudioContext;
-				tabsGaines[tabArray[0].id].streamer = stream;
-		        tabsGaines[tabArray[0].id].source = tabsGaines[tabArray[0].id].audioCtx.createMediaStreamSource(stream);
-		        tabsGaines[tabArray[0].id].nodeGain = tabsGaines[tabArray[0].id].audioCtx.createGain();
-		        tabsGaines[tabArray[0].id].source.connect(tabsGaines[tabArray[0].id].nodeGain);
-		        tabsGaines[tabArray[0].id].nodeGain.connect(tabsGaines[tabArray[0].id].audioCtx.destination);
-		        tabsGaines[tabArray[0].id].nodeGain.gain.value = parseFloat(gainLevels[tabsLevels[tabArray[0].id]]);
-				mySuperCallback();
-			});
-
+			showVolumeInTabFunc(1);
+			a.init(id, 100);
 		}
-		mySuperCallback();
 
-	}); 
+	});
+}
+var a = {
+	init: function(id, val){
+		tabsLevels[id] = parseFloat(val)/100;
+		if (Object.keys(tabsGaines).length < 6)
+			chrome.tabCapture.capture({
+			    audio: !0,
+			    video: !1
+			}, function(stream) {
+				tabsGaines[id] = {};
+				tabsGaines[id].audioCtx = new window.AudioContext;
+				tabsGaines[id].streamer = stream;
+		        tabsGaines[id].source = tabsGaines[id].audioCtx.createMediaStreamSource(stream);
+		        tabsGaines[id].nodeGain = tabsGaines[id].audioCtx.createGain();
+		        tabsGaines[id].source.connect(tabsGaines[id].nodeGain);
+		        tabsGaines[id].nodeGain.connect(tabsGaines[id].audioCtx.destination);
+		        tabsGaines[id].nodeGain.gain.value = tabsLevels[id];
+			});
+	},
+	deInit: function(id){
+		tabsGaines[id].streamer.getAudioTracks().forEach(function(track) {
+            track.stop();
+        });
+		Object.prototype.hasOwnProperty.call(tabsGaines, id) && tabsGaines[id].audioCtx.close()
+        .then(() => {
+			delete tabsGaines[id];
+			delete tabsLevels[id];
+        });
+	},
+	getTab: function(id){
+		if(tabsLevels.hasOwnProperty(id))
+			return true;
+		return false;
+	},
+	volume: function(id, val){
+		tabsGaines[id].nodeGain.gain.value = tabsLevels[id] = parseFloat(val)/100;
+	}
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-	mainClicker('up')
-});
 chrome.commands.onCommand.addListener(function (command) {
 	if (!hotkeysType[parseInt(command[command.length -1])-1])
 		return;
 	if (command.indexOf("toggle-up")!=-1)
-    	mainClicker('up')
+    	mainClicker(1)
     if (command.indexOf("toggle-down")!=-1)
-    	mainClicker('down')
+    	mainClicker(-1)
 });
 
 
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	console.log(request);
-	gainLevels = request.gainLevels;
 	hotkeysType = request.hotkeysType;
 });
+
+
+
+ chrome.extension.onConnect.addListener(function(port) {
+ 	// tabsGaines[tabArray[0].id].nodeGain.gain.value = parseFloat(gainLevels[tabsLevels[tabArray[0].id]]);
+      port.onMessage.addListener(function(e) {
+        a.getTab(e.id)?a.volume(e.id, e.val):a.init(e.id, e.val);
+      });
+      chrome.tabs.query({ currentWindow: true, active: true }, function (tabArray) {
+      	port.postMessage({tabsLevels: tabsLevels, curTab: tabArray[0]});
+  	  })
+ })
---
+++ custom/js/popup.js
@@ -0,0 +1,108 @@
+var tabsLevels = {};
+
+var port = chrome.extension.connect({
+	name: "Sample Communication"
+});
+function changeVolume(id, val){
+	port.postMessage({id: parseInt(id), val: val});
+}
+ // port.postMessage("Hi BackGround");
+port.onMessage.addListener(function(msg) {
+      tabsLevels = msg.tabsLevels;
+      console.log(tabsLevels);
+      $('#curContoller').attr('tabID', msg.curTab.id);
+chrome.tabs.query({audible: !0}, function(tabs){
+	var aTabs = '';
+	for (var i = tabs.length - 1; i >= 0; i--) {
+		if(tabsLevels.hasOwnProperty(tabs[i].id)){
+			// tabsLevels[tabs[i].id] = {volume: 100, boost: false};
+			console.log(tabsLevels[tabs[i].id])
+			if (tabsLevels[tabs[i].id]>1) {
+				var voll = 800;
+				var boost = 'checked=""';
+				var volDis = 'disabled=""';
+			}else{
+				var voll = tabsLevels[tabs[i].id]*100;
+				var boost = '';
+				var volDis = '';
+			}
+			aTabs = aTabs + '<tr id="'+tabs[i].id+'">'+
+				'<td class="switchTab"><img src="'+tabs[i].favIconUrl+'"></td>'+
+				'<td class="showControllers">'+
+					'<p class="range-field">'+
+						'<span class="tabName switchTab">'+tabs[i].title+'</span>'+
+						'<input type="range" value="'+voll+'" '+volDis+' min="0" max="100" />'+
+					'</p>'+
+				'</td>'+
+				'<td>'+
+					'<input type="checkbox" '+boost+' id="boost'+i+'" />'+
+					'<label for="boost'+i+'"></label>'+
+				'</td>'+
+			'</tr>';
+		}else{
+			aTabs = aTabs + '<tr id="'+tabs[i].id+'">'+
+				'<td class="switchTab switchTab"><img src="'+tabs[i].favIconUrl+'"></td>'+
+				'<td colspan="2">'+
+					'<p class="range-field switchTab">'+
+						'<span class="tabName">'+tabs[i].title+'</span>'+
+						'<b>Click to switch the tab</b>'+
+					'</p>'+
+				'</td>'+
+			'</tr>';
+		}
+	}
+	if (aTabs) {
+		$('.actived > tbody *').remove();
+		$('.actived > tbody').append(aTabs);
+	}
+	$('.switchTab').click(function(){
+		console.log('switch the tab');
+		chrome.tabs.update(parseInt($(this).closest('tr')[0].id), {active: true});
+	})
+	$('input[type="checkbox"]').change(function(){
+		if (this.id!="curBoost") {
+			var checkboxId = $(this).parent().parent()[0].id;
+		}else{
+			var checkboxId = msg.curTab.id;
+		}
+		if(this.checked){
+			tabsLevels[checkboxId] = $('#'+checkboxId+' input[type="range"], [tabID="'+checkboxId+'"] input[type="range"]').val();
+			$('#'+checkboxId+' input[type="range"], [tabID="'+checkboxId+'"] input[type="range"]').val(100);
+			$('#'+checkboxId+' input[type="range"], [tabID="'+checkboxId+'"] input[type="range"]').attr('disabled', true);
+			$('#'+checkboxId+' input[type="checkbox"], [tabID="'+checkboxId+'"] input[type="checkbox"]').attr('checked', "true");
+			changeVolume(checkboxId, 800);
+		}else{
+			// console.log(tabsLevels[checkboxId]);
+			$('#'+checkboxId+' input[type="range"], [tabID="'+checkboxId+'"] input[type="range"]').val(tabsLevels[checkboxId]);
+			$('#'+checkboxId+' input[type="range"], [tabID="'+checkboxId+'"] input[type="range"]').attr('disabled', false);
+			$('#'+checkboxId+' input[type="checkbox"], [tabID="'+checkboxId+'"] input[type="checkbox"]').removeAttr('checked');
+			changeVolume(checkboxId, tabsLevels[checkboxId]);
+		}
+	});
+	$(document).on('input', 'input[type="range"]', function() {
+		if (this.id!="currVolume") {
+			var tr = $(this).parent().parent().parent();
+			tabsLevels[tr[0].id] = $(this).val();
+			changeVolume(tr[0].id, $(this).val());
+			if (tr[0].id == msg.curTab.id)
+				$('#currVolume').val($(this).val())
+		}else{
+			changeVolume(msg.curTab.id, $(this).val());
+			$('#'+msg.curTab.id+' input[type="range"]').val($(this).val())
+		}
+	});
+
+
+
+
+
+
+
+	var hhhtml = '<h6 class="footer">'+
+				 	'Can you spend few seconds to <a href="https://goo.gl/c4TMmx" target="_blank">Rate <i class="material-icons">star_rate</i>my work</a> ?'+
+				 '</h6>';
+	if(Math.random() < 0.07)
+		$('body').append(hhhtml);
+
+});
+});
